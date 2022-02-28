78fef8c4cdb352d2024c82037c71f205d73498d0
LioraW <lcwdmg@umsystem.edu>
2022-02-27 20:20:40 -0600

    updated to use viewBinding

--- app/src/main/java/com/example/myrecyclerviewapp2/RecyclerAdapter.kt
+++ app/src/main/java/com/example/myrecyclerviewapp2/RecyclerAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myrecyclerviewapp2.databinding.CardLayoutBinding
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     //create some data
@@ -15,30 +16,27 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     private var details = arrayOf("Details 1", "Details 2","Details 3","Details 4","Details 5","Details 6","Details 7","Details 8","Details 9","Details 10",)
     private var images = arrayOf(R.drawable.android,R.drawable.android,R.drawable.android,R.drawable.android,R.drawable.android,R.drawable.android,R.drawable.android,R.drawable.android,R.drawable.android,R.drawable.android,)
 
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var itemImage : ImageView = itemView.findViewById(R.id.item_image)
-        var itemTitle : TextView = itemView.findViewById(R.id.item_title)
-        var itemDetails : TextView = itemView.findViewById(R.id.item_details)
-
-
+    inner class ViewHolder(val binding : CardLayoutBinding): RecyclerView.ViewHolder(binding.root) {
         init {
             itemView.setOnClickListener{
                 val position: Int = bindingAdapterPosition
-
                 Toast.makeText(itemView.context, "Clicked on ${titles[position]}", Toast.LENGTH_SHORT ).show()
             }
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false) //inflate the layout
-        return ViewHolder(v) //now recycler view has an instance of this viewHolder .... ???
+        val binding = CardLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) { //almost always the same. we have to pass the instance of the title, postion
-        holder.itemImage.setImageResource(images[position])
-        holder.itemTitle.text = titles[position]
-        holder.itemDetails.text = details[position]
-
+        with(holder){
+            with(binding){
+                itemImage.setImageResource(images[position])
+                itemTitle.text = titles[position]
+                itemDetails.text = details[position]
+            }
+        }
     }
 
     override fun getItemCount(): Int {
